25c708ddff1948828bb1d1a0f7d7eaf1bce96d61
coderx4032 <>
2022-06-21 05:29:25 +0000

    first commit firebase

---
+++ src/lib/firebase.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCLMLi3_LG5UBWklJNfRnY213ud-DQSPSI",
+  authDomain: "fir-sample-de5cd.firebaseapp.com",
+  projectId: "fir-sample-de5cd",
+  storageBucket: "fir-sample-de5cd.appspot.com",
+  messagingSenderId: "214300686660",
+  appId: "1:214300686660:web:1e977bbe5871865438ff11",
+  measurementId: "G-WHC60E9KJD"
+};
+
+firebase.initializeApp(firebaseConfig);
